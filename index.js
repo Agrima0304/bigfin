11960cc624061cdb551bdbfc21766c491768e2e4
agrima <agrima0304@gmail.com>
2023-11-10 15:18:27 +0530

    Bug Fixes Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const bodyParser = require('body-parser');
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
 
-
 const app = express();
 const PORT = process.env.PORT || 3556;
 
 app.use(bodyParser.json());
 
 const swaggerDocs = YAML.load("swagger.yaml")
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 let players = [];
 
+// POST /players – Creates a new entry for a player
 app.post('/players', (req, res) => {
   const { id, name, country, score } = req.body;
 
@@ -21,7 +22,7 @@ app.post('/players', (req, res) => {
   }
 
   if (name.length > 15) {
-    return res.status(400).json({ error: 'Name should have a max of 15 characters' });
+    return res.status(400).json({ error: 'Name should have a maximum of 15 characters' });
   }
 
   const player = { id, name, country, score };
@@ -29,6 +30,7 @@ app.post('/players', (req, res) => {
   res.status(201).json(player);
 });
 
+// PUT /players/:id – Updates the player attributes. Only name and score can be updated
 app.put('/players/:id', (req, res) => {
   const { id } = req.params;
   const { name, score } = req.body;
@@ -50,6 +52,7 @@ app.put('/players/:id', (req, res) => {
   res.json(player);
 });
 
+// DELETE /players/:id – Deletes the player entry
 app.delete('/players/:id', (req, res) => {
   const { id } = req.params;
 
@@ -58,12 +61,17 @@ app.delete('/players/:id', (req, res) => {
   res.json({ message: 'Player deleted successfully' });
 });
 
+// GET /players – Displays the list of all players in descending order
 app.get('/players', (req, res) => {
+  if (!Array.isArray(players)) {
+    players = [];
+  }
+
   const sortedPlayers = players.sort((a, b) => b.score - a.score);
   res.json(sortedPlayers);
 });
 
-
+// GET /players/rank/:val – Fetches the player ranked “val”
 app.get('/players/rank/:val', (req, res) => {
   const { val } = req.params;
   const rankedPlayer = players[val - 1];
@@ -75,7 +83,7 @@ app.get('/players/rank/:val', (req, res) => {
   res.json(rankedPlayer);
 });
 
-
+// GET /players/random – Fetches a random player
 app.get('/players/random', (req, res) => {
   const randomIndex = Math.floor(Math.random() * players.length);
   const randomPlayer = players[randomIndex];
